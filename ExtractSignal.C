a38164cb85e14a91eb391bf66c84a445599d8f48
YitaoWU <torrence@mail.ustc.edu.cn>
2018-11-22 20:10:49 +0800

    NEW method for signal/background estimation & fix params order by TF1::SetParName(s)

--- ExtractSignal.C
+++ ExtractSignal.C
@@ -1,6 +1,12 @@
 // Macro for result post-processing
 
-// Fit Me+e- (invariant mass of di-electron)
+// Fit J/psi signal in Me+e- spectrum (invariant mass of di-electron)
+
+#define DEBUG
+
+TF1* jpsi = NULL;
+TF1* bkg = NULL;
+TF1* total = NULL;
 
 TLegend* yatoLegend(){
 		TLegend* yLgd= new TLegend(0.20,0.15,0.6,0.35);
@@ -12,41 +18,79 @@ TLegend* yatoLegend(){
 		return yLgd;
 }
 
-TVirtualPad* DrawFitResult(TH1* invmass, TF1* total, TF1* jpsi, TF1* bkg){
-  TVirtualPad* fCanvas = gPad;
-  if(!fCanvas){
-    fCanvas = new TCanvas("cAna", "Canvas for Jpsi signal extraction", 800, 600);
+// Input mass range for estimation of signal & background
+int SelectSignalRegion(Double_t mlow, Double_t mup, Double_t width = 0.04){
+  // Denominator check
+  if( width < 1e-6 ){
+    cout << "[+] WARNNING - Bin width given may be too small : " << width << endl;
   }
+  // Fit result check
+  auto fitter = TVirtualFitter::GetFitter();
+  if(!fitter){
+    cout << "[X] ERROR - No fit result found." << endl;
+    return 1;
+  }
+  // Calculate signal & background number with TF1::Integral
+  Double_t Ntotal = total->Integral(mlow, mup) / width;
+  Double_t Njpsi = jpsi->Integral(mlow, mup) / width;
+  Double_t Nbkg = bkg->Integral(mlow, mup) / width;
+  // Integral error with sub-covariance matrix
+  Double_t errTot = total->IntegralError(mlow, mup) / width;
+  
+  TMatrixDSym* covTot = new TMatrixDSym(0, 7, fitter->GetCovarianceMatrix());
+  TMatrixDSym covJpsi = covTot->GetSub(0,4,0,4);
+  TMatrixDSym covBkg = covTot->GetSub(5,7,5,7);
+#ifdef DEBUG
+  covTot->Print();
+  covJpsi.Print();
+  covBkg.Print();
+#endif
+  delete covTot;
 
-  return fCanvas;
+  jpsi->SetParErrors(total->GetParErrors());
+  bkg->SetParErrors(total->GetParErrors() + 5);
+  Double_t errJpsi = jpsi->IntegralError(mlow, mup, jpsi->GetParameters(), covJpsi.GetMatrixArray()) / width;
+  if(errJpsi < 1.)
+    cout << "[+] WARNNING - Njpsi error would be too small : " << errJpsi << endl;
+  Double_t errBkg = bkg->IntegralError(mlow, mup, bkg->GetParameters(), covBkg.GetMatrixArray()) / width;
+  if(errBkg < 1.)
+    cout << "[+] WARNNING - Nbackground error would be too small : " << errBkg << endl;
+  // Result output
+  cout << "[-] INFO - Estimation of J/psi signal & background in mass range [" << mlow << ", " << mup << "] (GeV/c^2)" << endl;
+  cout << "--> Total:      " << Ntotal << " +/- " << errTot << endl;
+  cout << "--> Signal:     " << Njpsi << " +/- " << errJpsi << endl;
+  cout << "--> Background: " << Nbkg << " +/- " << errBkg << endl;
+  return 0;
 }
 
 int ExtractSignal(TH1* invmass = NULL){
   // Crystalball Function = Gaus + X^n
     // Parameters : \alpha = break point, n, \sigma = width, \mu = peak
     // Unit : Y = Ncount/0.04, X=GeV/c^2
-  auto jpsi = new TF1("fJpsi", "[A]*ROOT::Math::crystalball_function(x,[#alpha],[n],[#sigma],[#mu])",1.,5.);
+  jpsi = new TF1("fJpsi", "[0]*ROOT::Math::crystalball_function(x,[1],[2],[3],[4])",1.,5.);
   jpsi->SetLineColor(kBlack);
   jpsi->SetLineWidth(2);
   jpsi->SetLineStyle(2);
   jpsi->SetNpx(1000);
 
   // Backgroud Function - Pol2
-  auto bkg = new TF1("fBkg", "[a0]+[a1]*x+[a2]*x^2", 1., 5.);
+  bkg = new TF1("fBkg", "[0]+[1]*x+[2]*x^2", 1., 5.);
   bkg->SetLineColor(kGreen);
   bkg->SetLineWidth(2);
   bkg->SetLineStyle(2);
   bkg->SetNpx(1000);
 
   // Total function for fitting
-  auto total = new TF1("fTot","fJpsi+fBkg", 1., 5.);
+  total = new TF1("fTot","fJpsi+fBkg", 1., 5.);
+  total->SetParNames("A", "#alpha", "n", "#sigma", "#mu",
+    "a0", "a1", "a2");
   total->SetLineColor(kRed);
   total->SetLineWidth(3);
   total->SetNpx(1000);
     // Jpsi parameter
   total->SetParameter("A", 116);
   total->SetParameter("#alpha", 0.3);
-  total->SetParameter("n", 2.4e8);
+  total->SetParameter("n", 1.);
   total->SetParameter("#sigma", 0.04);
   total->SetParameter("#mu", 3.1);
     // Background parameter
@@ -61,9 +105,12 @@ int ExtractSignal(TH1* invmass = NULL){
   invmass->SetLineColor(kBlue);
   invmass->SetMarkerColor(kBlue);
   invmass->SetMarkerStyle(20);
-
+    // Fit with bin integration and return fit result
   invmass->Fit(total, "IS", "", 1.5, 4.5);
+    // Draw marker & error bar (with empty bins)
   invmass->Draw("PE0");
+
+  // Canvas configuration
   if(gPad){
     invmass->GetYaxis()->SetRangeUser(0,300);
     invmass->GetYaxis()->SetTitle("N_{pairs} / 0.04 GeV/c^{2}");
@@ -76,23 +123,23 @@ int ExtractSignal(TH1* invmass = NULL){
 
   total->Draw("same");
 
-  jpsi->SetParameter("A", total->GetParameter("A"));
-  jpsi->SetParameter("#alpha", total->GetParameter("#alpha"));
-  jpsi->SetParameter("n", total->GetParameter("n"));
-  jpsi->SetParameter("#sigma", total->GetParameter("#sigma"));
-  jpsi->SetParameter("#mu", total->GetParameter("#mu"));
+  jpsi->SetParameter(0, total->GetParameter("A"));
+  jpsi->SetParameter(1, total->GetParameter("#alpha"));
+  jpsi->SetParameter(2, total->GetParameter("n"));
+  jpsi->SetParameter(3, total->GetParameter("#sigma"));
+  jpsi->SetParameter(4, total->GetParameter("#mu"));
   jpsi->Draw("same");
   
-  bkg->SetParameter("a0", total->GetParameter("a0"));
-  bkg->SetParameter("a1", total->GetParameter("a1"));
-  bkg->SetParameter("a2", total->GetParameter("a2"));
+  bkg->SetParameter(0, total->GetParameter("a0"));
+  bkg->SetParameter(1, total->GetParameter("a1"));
+  bkg->SetParameter(2, total->GetParameter("a2"));
   bkg->Draw("same");
 
   // Setup for legend
   auto lgd = yatoLegend();
   lgd->AddEntry(invmass, "e^{+}e^{-} signal");
   lgd->AddEntry(total,"Total fit");
-  lgd->AddEntry(bkg,"Bkg fit (pol2)");
+  lgd->AddEntry(bkg,"Background fit (pol2)");
   lgd->AddEntry(jpsi,"Signal fit (Crystal-Ball)");
   lgd->Draw("same");
 
