382d9568b810b2f97a61b5e7a46492c8d9018555
ytwu <torrence@mail.ustc.edu.cn>
2018-11-26 22:17:04 +0800

    UPDATE pp 13TeV J/psi p_{t} fitted Levy function from ALICE 2017 result

--- JpsiToyMC.C
+++ JpsiToyMC.C
@@ -1,5 +1,5 @@
 // Toy MC for J/psi decay
-#define DEBUG
+//#define DEBUG
 
 // Require ROOT6
 #include "TLorentzVector.h"
@@ -7,6 +7,22 @@
 // Use Float_t instead of double to save disk size
 const Float_t MASS_JPSI = 3.096; // GeV/c^2
 const Float_t MASS_ELECTRON = 511e-6; // GeV/c^2
+
+// Levy-Tsallis Function for Pt 
+// Variable : Pt - Traverse Momentum
+// Parameters : dNdy, n, C, mass
+Double_t LevyFCN (Double_t* x, Double_t* par) {
+	Double_t pt = x[0];
+	Double_t dNdy = par[0];
+	Double_t n = par[1];
+	Double_t C = par[2];
+	Double_t mass = par[3];
+	Double_t mt = TMath::Sqrt(pt*pt+mass*mass);
+	
+	return pt * dNdy * (n-1) * (n-2) / (n*C * (n*C + mass * ( n -2))) \
+		* TMath::Power( 1 +( mt -mass) / (n*C), -n);
+	
+}
 
 /*
 * Methods from LIU, Zhen (刘圳)
@@ -52,19 +68,52 @@ TLorentzVector getJpsi(TF1 *fPt, Float_t pt_lower, Float_t pt_upper)
 }
 // END - Methods from LIU, Zhen (刘圳)
 
-int JpsiToyMC(){
+int JpsiToyMC(Int_t nEvent = 1e3, TString output_dir = "."){
   gRandom->SetSeed(time(NULL));
 
   // Au-Au 200 GeV/c
-  TF1 *fPt = new TF1("fPt", "(3.4948*TMath::Power(TMath::Exp((-0.395305)*x)+x/2.91793,(-8.46161)))*4*TMath::Pi()*x", 0, 20);
+  //TF1 *fPt = new TF1("fPt", "(3.4948*TMath::Power(TMath::Exp((-0.395305)*x)+x/2.91793,(-8.46161)))*4*TMath::Pi()*x", 0, 20);
+  const Double_t PT_LOWER = 0.;
+  const Double_t PT_UPPER = 30.; // GeV/c
+  TF1* fPt = new TF1("fLevy", LevyFCN, PT_LOWER, PT_UPPER, 4);
+  // pp 13TeV - Levy fit with ALICE-2017
+  fPt->SetParName(0, "dN/dy");
+  fPt->SetParName(1, "C");
+  fPt->SetParName(2, "n");
+  fPt->SetParName(3, "m_{0}");
+  fPt->SetParameters(7413., 7.79, 0.7053, MASS_JPSI);
 
-  TLorentzVector jpsi = getJpsi(fPt, 0., 10.);
-  TLorentzVector eplus = twoBodyDecay(jpsi, MASS_ELECTRON);
-  TLorentzVector eminus = jpsi - eplus;
+  TFile* f = new TFile(output_dir+"/JpsiDecay.root", "RECREATE");
+  TTree* tree = new TTree("event", "J/psi decay event");
+  TLorentzVector* jpsi = new TLorentzVector;
+  TLorentzVector& obj_jpsi = *jpsi;
+  TLorentzVector* eplus = new TLorentzVector;
+  TLorentzVector& obj_eplus = *eplus;
+  TLorentzVector* eminus = new TLorentzVector;
+  TLorentzVector& obj_eminus = *eminus;
+  tree->Branch("jpsi","TLorentzVector",&jpsi);
+  tree->Branch("eplus","TLorentzVector",&eplus);
+  tree->Branch("eminus","TLorentzVector",&eminus);
+  for (Int_t iev = 0; iev < nEvent ; iev++){
+    obj_jpsi = getJpsi(fPt, PT_LOWER, PT_UPPER);
+    obj_eplus = twoBodyDecay(obj_jpsi, MASS_ELECTRON);
+    obj_eminus = obj_jpsi - obj_eplus;
 #ifdef DEBUG
-  jpsi.Print();
-  eplus.Print();
-  eminus.Print();
+    jpsi->Print();
+    eplus->Print();
+    eminus->Print();
 #endif // DEBUG
+    tree->Fill();
+    if(iev % (nEvent / 25) == 0){
+      cout << "[-] Progress : " << int(iev/1./nEvent*100) 
+        << "% (" << iev << "/" << nEvent << ")" << endl;
+    }
+  }
+  cout << "[-] INFO - Event loop finished : " << nEvent << " events" << endl;
+#ifdef DEBUG
+  tree->Print();
+#endif // DEBUG
+  f->Write();
+  f->Close();
   return 0;
 }
