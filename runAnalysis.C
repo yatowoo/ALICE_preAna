5e817e51d705a93382b2b22f37c603bbf4db5ea6
ytwu <torrence@mail.ustc.edu.cn>
2018-12-09 08:04:26 +0100

    UPDATE configuration for MB

--- runAnalysis.C
+++ runAnalysis.C
@@ -33,9 +33,9 @@ void runAnalysis(TString mode="local", TString work_dir="16l_Full_CJ_MB-EG1-EG2"
     // TASK - PID response
     gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C");
     AddTaskPIDResponse();
-    // TASK - EG2 from C. Jahnke
+    // TASK - MB from C. Jahnke
     gROOT->LoadMacro("AddTask_cjahnke_JPsi.C");
-    AddTask_cjahnke_JPsi("16l", 4, kFALSE, "ConfigJpsi_cj_pp", kFALSE, kTRUE, 0);
+    AddTask_cjahnke_JPsi("16l", 100, kFALSE, "ConfigJpsi_cj_pp", kFALSE, kTRUE, 0);
 
     if(!mgr->InitAnalysis()) return;
     mgr->SetDebugLevel(2);
